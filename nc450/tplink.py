b1281d28a21f6cdf9ff7c180a65851087620d10a
Tom French <tom@tomfren.ch>
2019-04-20 19:33:04 +0100

    Moved endpoint dictionary to const.py

--- nc450/tplink.py
+++ nc450/tplink.py
@@ -1,33 +1,34 @@
-import requests
 import base64
 import time
 
+import requests
+
+from const import (
+    GET_CLOUD,
+    GET_ETHERNET,
+    GET_LED,
+    GET_MOTION,
+    GET_OSD,
+    GET_PTZ,
+    GET_SYSTEM_INFORMATION,
+    GET_VIDEO,
+    GET_WIFI,
+    GET_WIFI_STATUS,
+    LOGIN,
+    LOGOUT,
+    REBOOT,
+    SCAN_WIFI,
+    SET_ETHERNET,
+    SET_LED,
+    SET_MOTION,
+    SET_OSD,
+    SET_PTZ,
+    SET_TURN,
+    SET_WIFI,
+)
+
 
 class NC450:
-
-    end_points = {
-        "LOGIN": "login.fcgi",
-        "LOGOUT": "logout.fcgi",
-        "GET_SYSTEM_INFORMATION": "getsysinfo.fcgi",
-        "GET_LED": "ledgetting.fcgi",
-        "SET_LED": "ledsetting.fcgi",
-        "GET_CLOUD": "get_cloud.fcgi",
-        "GET_VIDEO": "getvideosetting.fcgi",
-        "GET_OSD": "getosdandtimedisplay.fcgi",
-        "SET_OSD": "setosdandtimedisplay.fcgi",
-        "SET_TURN": "setTurnDirection.fcgi",
-        "GET_MOTION": "mdconf_get.fcgi",
-        "SET_MOTION": "mdconf_set.fcgi",
-        "GET_WIFI_STATUS": "wireless_status.fcgi",
-        "SCAN_WIFI": "wireless_scan.fcgi",
-        "GET_WIFI": "wireless_get.fcgi",
-        "SET_WIFI": "wireless_set.fcgi",
-        "GET_ETHERNET": "netconf_get.fcgi",
-        "SET_ETHERNET": "netconf_set.fcgi",
-        "GET_PTZ": "getPtzVelocity.fcgi",
-        "SET_PTZ": "setPtzVelocity.fcgi",
-        "REBOOT": "reboot.fcgi",
-    }
 
     valid_directions = ["n", "nw", "w", "sw", "s", "se", "e", "ne", "c"]
 
@@ -41,7 +42,7 @@ class NC450:
         self.session = requests.session()
 
     def _build_endpoint_url(self, endpoint):
-        return "http://" + self.url + "/" + NC450.end_points[endpoint]
+        return "http://" + self.url + "/" + endpoint
 
     def get(self, endpoint):
         url = self._build_endpoint_url(endpoint)
@@ -58,35 +59,35 @@ class NC450:
 
     def login(self, username="admin", password="admin"):
         login_data = {"Username": username, "Password": base64.b64encode(password)}
-        response = self.post("LOGIN", login_data)
+        response = self.post(LOGIN, login_data)
         print(response.json())
         self.token = response.json()["token"]
 
     def logout(self):
-        response = self.post("LOGOUT")
+        response = self.post(LOGOUT)
         self.token = None
 
     def system_info(self):
-        response = self.get("GET_SYSTEM_INFORMATION")
+        response = self.get(GET_SYSTEM_INFORMATION)
         return response.json()
 
     def reboot(self):
-        self.post("REBOOT", {"token": self.token})
+        self.post(REBOOT, {"token": self.token})
 
     #### Video ####
 
     def video(self):
-        response = self.get("GET_VIDEO")
+        response = self.get(GET_VIDEO)
         return response.json()
 
     #### On-Screen Display ####
 
     def osd(self):
-        response = self.get("GET_OSD")
+        response = self.get(GET_OSD)
         return response.json()
 
     def set_osd_options(self, osd_data):
-        self.post("SET_OSD", osd_data)
+        self.post(SET_OSD, data=osd_data)
 
     def set_osd_visibility(self, osd_state):
         osd_data = {"osd_enable": osd_state}
@@ -106,78 +107,78 @@ class NC450:
             raise ValueError("attempting to move camera in invalid direction.")
         else:
             turn_data = {"direction": direction, "operation": operation}
-            self.post("SET_TURN", turn_data)
+            self.post(SET_TURN, turn_data)
             if operation == "start" and direction is not "c":
                 time.sleep(timestep)
                 self.turn(direction, timestep, "stop")
 
     def velocity(self):
-        response = self.get("GET_PTZ")
+        response = self.get(GET_PTZ)
         return response.json()
 
     def set_velocity(self, velocity):
         if velocity not in [0, 1, 2]:
             raise ValueError("Attempting to set velocity to invalid value")
         velocity_data = {"value": velocity}
-        self.post("SET_PTZ", velocity_data)
+        self.post(SET_PTZ, velocity_data)
 
     #### Motion Detection ####
 
     def motion_detection(self):
-        response = self.get("GET_MOTION")
+        response = self.get(GET_MOTION)
         return response.json()
 
     def set_md_status(self, status):
         md_status = {"is_enable": status}
-        self.post("SET_MOTION", md_status)
+        self.post(SET_MOTION, md_status)
 
     def set_md_sensitivity(self, status):
         # add value validation
         md_status = {"precision": status}
-        self.post("SET_MOTION", md_status)
+        self.post(SET_MOTION, md_status)
 
     #### LED ####
 
     def led_status(self):
-        response = self.get("GET_LED")
+        response = self.get(GET_LED)
         return response.json()
 
     def set_led_status(self, status):
         if status not in [0, 1]:
             raise ValueError("Attempting to set led_status to invalid value")
         led_data = {"enable": status}
-        response = self.post("SET_LED", led_data)
+        response = self.post(SET_LED, led_data)
 
     #### WiFi ####
 
     def get_wifi_status(self):
-        response = self.get("GET_WIFI_STATUS")
+        response = self.get(GET_WIFI_STATUS)
         return response.json()
 
     def scan_wifi(self):
-        response = self.post("SCAN_WIFI")
+        response = self.post(SCAN_WIFI)
         return response.json()
 
     def wifi_settings(self):
-        response = self.get("GET_WIFI")
+        response = self.get(GET_WIFI)
         return response.json()
 
     def set_wifi_settings(self, data):
-        response = self.post("SET_WIFI", data)
+        response = self.post(SET_WIFI, data)
         return response.json()
 
     #### Ethernet ####
 
     def ethernet_settings(self):
-        response = self.get("GET_ETHERNET")
+        response = self.get(GET_ETHERNET)
         return response.json()
 
     def set_ethernet_settings(self, data):
-        response = self.post("SET_ETHERNET", data)
+        response = self.post(SET_ETHERNET, data)
         return response.json()
 
     #### Notification ####
 
     def ftp_smtp_settings(self):
-        response = self.get("GET_MOTION")
+        response = self.get(GET_MOTION)
         return response.json()
